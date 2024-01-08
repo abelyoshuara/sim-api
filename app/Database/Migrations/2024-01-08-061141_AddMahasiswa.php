cbbca8c9d5a174e9e526de9c241d1d97ffffe8ea
Abel Yoshuara <abelyoshuara@gmail.com>
2024-01-08 14:50:38 +0800

    setup migration

---
+++ app/Database/Migrations/2024-01-08-061141_AddMahasiswa.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddMahasiswa extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'nim' => [
+                'type'       => 'CHAR',
+                'constraint' => '9',
+            ],
+            'jurusan' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'foto' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'null'       => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('mahasiswa');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('mahasiswa');
+    }
+}

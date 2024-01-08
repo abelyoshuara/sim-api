f60fb8a1d72ed45456b30e3ef06ffa285f6947d5
Abel Yoshuara <abelyoshuara@gmail.com>
2024-01-08 17:12:42 +0800

    get all mahasiswa feature

---
+++ app/Models/MahasiswaModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MahasiswaModel extends Model
+{
+    protected $table            = 'mahasiswa';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = \App\Entities\Mahasiswa::class;
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['nama', 'nim', 'jurusan', 'foto'];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [
+        'nama' => 'required|alpha_space',
+        'nim' => 'required|is_unique[mahasiswa.nim,id,{id}]',
+        'jurusan' => 'required'
+    ];
+    // protected $validationMessages   = [];
+    // protected $skipValidation       = false;
+    // protected $cleanValidationRules = true;
+
+    // Callbacks
+    // protected $allowCallbacks = true;
+    // protected $beforeInsert   = [];
+    // protected $afterInsert    = [];
+    // protected $beforeUpdate   = [];
+    // protected $afterUpdate    = [];
+    // protected $beforeFind     = [];
+    // protected $afterFind      = [];
+    // protected $beforeDelete   = [];
+    // protected $afterDelete    = [];
+}

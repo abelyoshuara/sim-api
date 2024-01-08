8bd2e1392226d95072a78a5793fef93bcdb0a6a7
Abel Yoshuara <abelyoshuara@gmail.com>
2024-01-08 15:03:29 +0800

    update migration

--- app/Database/Migrations/2024-01-08-061141_AddMahasiswa.php
+++ app/Database/Migrations/2024-01-08-061141_AddMahasiswa.php
@@ -32,6 +32,18 @@ class AddMahasiswa extends Migration
                 'constraint' => '100',
                 'null'       => true,
             ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ]
         ]);
         $this->forge->addKey('id', true);
         $this->forge->createTable('mahasiswa');
